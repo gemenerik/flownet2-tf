ad9299deb15017df1b9b1cf8a2e1887cb6675619
gemenerik <rikjbouwmeester@gmail.com>
2020-11-23 10:56:03 +0100

    initial commit

---
+++ src/tinyflownet/train.py
@@ -0,0 +1,19 @@
+from ..dataloader import load_batch
+from ..dataset_configs import FLYING_CHAIRS_DATASET_CONFIG
+from ..training_schedules import LONG_SCHEDULE
+from .tinyflownet import TinyFlowNet
+
+# Create a new network
+net = TinyFlowNet()
+
+# Load a batch of data
+input_a, input_b, flow = load_batch(FLYING_CHAIRS_DATASET_CONFIG, 'sample', net.global_step)
+
+# Train on the data
+net.train(
+    log_dir='./logs/tinyflownet_sample',
+    training_schedule=LONG_SCHEDULE,
+    input_a=input_a,
+    input_b=input_b,
+    flow=flow
+)
